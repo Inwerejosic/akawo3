d54dc9a96dfdf50e469e0dd6201e55dc3e269de5
inwerejosic <inwerejosic@protonmail.com>
2026-07-22 15:37:39 +0100

    Message: Included my first real integration test.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,36 @@
-use actix_web::{App, HttpRequest, HttpServer, Responder, web};
+use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello {}!", &name)
 }
+
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(greet))
+            .route("/health", web::get().to(health_check))
             .route("/{name}", web::get().to(greet))
     })
     .bind("127.0.0.1:8000")?
     .run()
     .await
+}
+
+
+#[cfg(test)]
+mod tests {
+    use crate::health_check;
+
+    #[tokio::test]
+    async fn health_check_success() {
+        let response = health_check().await;
+
+        assert!(response.status().is_success())
+    }
 }
